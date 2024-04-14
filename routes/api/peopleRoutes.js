cc26b5b3458f7fb912c27bb45da5d15d218e005d
victorgarrido1 <vcgarr94@gmail.com>
2024-04-13 22:00:03 -0700

    person and post route seem to work

--- routes/api/peopleRoutes.js
+++ routes/api/peopleRoutes.js
@@ -1,30 +1,42 @@
 const router = require('express').Router();
-const { Bags, People, Location  } = require('../../models')
+const { Bags, People, Location } = require('../../models');
 
 // GET method route
 router.get('/', async (req, res) => {
+  try {
+    const PeopleData = await People.findAll();
+    res.status(200).json(PeopleData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  /// find by Individual person
+  router.get('/:id', async (req, res) => {
     try {
-        const PeopleData  = await People.findAll();
-        res.status(200).json(PeopleData);
-    }catch(err) {
-        res.status(500).json(err);
+      const PeopleData = await People.findByPk(req.params.id);
+      res.status(200).json(PeopleData);
+    } catch (err) {
+      res.status(500).json(err);
     }
-
-
-    /// find by Individual person 
-    router.get('/:id', async (req, res) => {
-        try {
-            const PeopleData = await People.findByPk(req.params.id);
-            res.status(200).json(PeopleData);
-        } catch(err) {
-            res.status(500).json(err);
-        }
-    })
+  });
 });
+
+//create a new person route
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    try {
+        const PeopleData = await People.create(req.body);
+        res.status(200).json(PeopleData);
+    } catch (err) {
+        res.status(500).json(err);        
+    }
+});
+
+
 
 // Error route
 router.get('/error', (req, res) => {
-    res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: 'Route not found' });
 });
 
 module.exports = router;
